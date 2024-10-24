76c7f4079c331e864bd8a9dbcec1eb1f9ffbaadb
Daniel Toquica <jorgeeyepesv@gmail.com>
2024-10-24 15:19:24 -0500

    register-with-team

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
+import Team from '../models/teamModel.js';
+import generateInvitationCode from '../utils/generateCode.js';
 
 // Registrar usuario
 export const register = async (req, res) => {
@@ -90,6 +92,56 @@ export const refreshToken = async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({ message: 'Error refreshing token', error: error.message });
+  }
+};
+
+export const registerAndCreateTeam = async (req, res) => {
+  const { username, password, role, teamName } = req.body;
+
+  try {
+    // Verificar si ya existe un usuario con el mismo username
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Hashear la contraseña
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Crear el nuevo usuario
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      role: role || 'student',
+    });
+
+    await newUser.save();
+
+    // Generar código de invitación
+    const codeInvitation = generateInvitationCode();
+
+    // Crear equipo y asignar al usuario como líder
+    const newTeam = new Team({
+      name: teamName, // El nombre del equipo lo pasamos en la solicitud
+      codeInvitation,
+      leader: newUser._id,
+      users: [newUser._id],
+    });
+
+    await newTeam.save();
+
+    // Actualizar al usuario para que sea líder y asignarle el equipo
+    newUser.team = newTeam._id;
+    newUser.isLeader = true;
+    await newUser.save();
+
+    res.status(201).json({
+      message: 'User and team created successfully',
+      user: newUser,
+      team: newTeam,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering user and creating team', error: error.message });
   }
 };
 
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { register, login, refreshToken } from '../controllers/authController.js';
+import { register, login, refreshToken, registerAndCreateTeam } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/refresh-token', refreshToken);
+router.post('/register-with-team', registerAndCreateTeam);
 
 export default router;
